4c66328943b16ffbf80a2b1d12df1448c43b82a4
Diegorra <diegoars@ext.inditex.com>
2024-09-30 16:32:40 +0200

    adding mongoose

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Contact = require('./models/contact')
 const app = express()
 
 
@@ -13,53 +15,27 @@ app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postInfo'))
 
-  
-let contacts = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${contacts.length} people</p><p>${Date()}</p>`)
 })
 
-
 app.get('/api/persons', (request, response) => {
+  Contact.find({}).then(contacts => {
     response.json(contacts)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  let id = Number(request.params.id)
-  let contact = contacts.find(contact => contact.id === id)
-  
-  if (contact) {
+  Contact.fincById(request.params.id).then(contact => {
     response.json(contact)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  let id = Number(request.params.id)
-  contacts = contacts.filter(contact => contact.id !== id)
+  // let id = Number(request.params.id)
+  // localContacts = localContacts.filter(contact => contact.id !== id)
+  // Contact.deleteOne()
 
   response.status(204).end()
 })
@@ -72,18 +48,14 @@ app.post('/api/persons', (request, response) => {
       error: "You must add a contact with a name or number"})
   }
 
-  if (contacts.some(contact => contact.name === newContact.name)) {
-    return response.status(404).json({ 
-      error: `${newContact.name} is already added to the phonebook`})
-  }
-
-  let contact = {
-    id: Math.floor(Math.random() * 99),
+  let contact = new Contact({
     name: newContact.name,
     number: newContact.number
-  }
-  contacts = contacts.concat(contact)
-  response.json(contact)
+  })
+  
+  contact.save().then(savedContact => {
+    response.json(savedContact)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
@@ -92,7 +64,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
